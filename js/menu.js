ecc02a6aefec125edc51978f89a27462fd32b09a
LolerSwager <53053540+LolerSwager@users.noreply.github.com>
2020-09-25 10:20:12 +0200

    update

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,9 @@
-// assign function to onclick property of checkbox
 document.getElementById('checkbox').onclick = function() {
-    // call toggleSub when checkbox clicked
-    // toggleSub args: checkbox clicked on (this), id of element to show/hide
     toggleSub(this, 'ul');
 };
 
-// called onclick of checkbox
 function toggleSub(box, id) {
-    // get reference to related content to display/hide
-    var el = document.getElementById(id);
+    let el = document.getElementById(id);
     
     if ( box.checked ) {
         el.style.display = 'block';
